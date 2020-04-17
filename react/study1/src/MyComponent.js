bc78f0d84fd81ed4d79bd3f9734a43af112a6933
miladiashe <dkdud0122@naver.com>
2020-04-17 22:05:34 +0900

    children, 비구조화 할당

--- react/study1/src/MyComponent.js
+++ react/study1/src/MyComponent.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const MyComponent = () => {
-    return <div>나의 새로운 컴포넌트</div>
+const MyComponent = props => {
+    const { name, children } = props;
+    return <div>
+        안녕하세요. 제 이름은 {name}입니다.<br />
+        children 값은 {children}입니다!
+    </div>
+};
+
+MyComponent.defaultProps = {
+    name: '기본 이름'
 };
 
 export default MyComponent;
